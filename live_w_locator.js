130fe883075ca01d27d19536d68aea32aa5a1c89
Chuporceeta <xabier.sienra@gmail.com>
2022-11-22 15:20:08 -0500

    Update live_w_locator.js

--- live_w_locator.js
+++ live_w_locator.js
@@ -31,7 +31,6 @@ $(function() {
             inputStream: {
                 type : "LiveStream",
                 constraints: {
-
                     facingMode: "environment",
                 }
             },
